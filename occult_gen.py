0f9704e18fc4fb8a93ff89b1b0b93edbf0406b62
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2024-02-16 00:14:13 +0100

    prefixes

--- occult_gen.py
+++ occult_gen.py
@@ -1,15 +1,29 @@
 import sys
 
+stack = []
+
 def _dfs(item, depth = 0):
     print(" " * depth * 2, end = "")
+    stack.append(item[0])
     print(item[0])
     for i in range(1, len(item)):
         if type(item[i]) is tuple:
             _dfs(item[i], depth + 1)
+        else:
+            stack.append(item[i])
+
+def _process():
+    if not stack:
+        return
+    while stack:
+        item = stack.pop()        
 
 def items(ast):
-    if ast[0] != "PROGRAM":
+    global stack
+    if ast[0] != "OCCULT_PROGRAM":
         print("Programma invalidum ☠️ ")
         sys.exit(1)
     for item in ast[1]:
         _dfs(item, 0)
+        _process()
+        stack = []
--- occult_parser.py
+++ occult_parser.py
@@ -11,7 +11,7 @@ def p_program(p):
     '''
     program : statement_list
     '''
-    p[0] = ('PROGRAM', p[1])
+    p[0] = ('OCCULT_PROGRAM', p[1])
 
 def p_statement_list(p):
     '''
@@ -28,94 +28,94 @@ def p_array(p):
     '''
     expression : LSQUARE sequence RSQUARE
     '''
-    p[0] = ('ARRAY', p[2])
+    p[0] = ('OCCULT_ARRAY', p[2])
 
 def p_sequence_single(p):
     '''
     sequence : expression
     '''
-    p[0] = ('ONE-VALUE-ARRAY', p[1])
+    p[0] = ('OCCULT_ONE-VALUE-ARRAY', p[1])
 
 def p_sequence_multiple(p):
     '''
     sequence : expression COMMA sequence
     '''
-    p[0] = ('MULTI-VALUE-ARRAY', p[1], p[3])
+    p[0] = ('OCCULT_MULTI-VALUE-ARRAY', p[1], p[3])
 
 def p_statement_equals(p):
     '''
     statement : NAME EQUALS expression END
     '''
-    p[0] = ('VARIABLE-ASSIGNMENT', p[1], p[3])
+    p[0] = ('OCCULT_VARIABLE-ASSIGNMENT', p[1], p[3])
 
 def p_statement_exit(p):
     '''
     statement : EXIT
     '''
-    p[0] = ('EXIT', None)
+    p[0] = ('OCCULT_EXIT', None)
 
 def p_statement_out(p):
     '''
     statement : PRINT expression END
     '''
-    p[0] = ('STATEMENT-OUT', p[2])
+    p[0] = ('OCCULT_STATEMENT-OUT', p[2])
 
 def p_statement_expr(p):
     'statement : expression END'
-    p[0] = ('STATEMENT-EXPRESSION', p[1]) 
+    p[0] = ('OCCULT_STATEMENT-EXPRESSION', p[1]) 
 
 def p_expression_concatenate(p):
     'expression : expression CONCATENATE expression'
-    p[0] = ('STATEMENT-CONCAT', p[1], p[3])
+    p[0] = ('OCCULT_STATEMENT-CONCAT', p[1], p[3])
 
 def p_expression_plus(p):
     'expression : expression PLUS term'
-    p[0] = ('EXPRESSION-PLUS', p[1], p[3])
+    p[0] = ('OCCULT_EXPRESSION-PLUS', p[1], p[3])
 
 def p_expression_minus(p):
     'expression : expression MINUS term'
-    p[0] = ('EXPRESSION-MINUS', p[1], p[3])
+    p[0] = ('OCCULT_EXPRESSION-MINUS', p[1], p[3])
     p[0] = p[1] - p[3]
 
 def p_expression_term(p):
     'expression : term'
-    p[0] = ('EXPRESSION-TERM', p[1])
+    p[0] = ('OCCULT_EXPRESSION-TERM', p[1])
 
 def p_term_times(p):
     'term : term TIMES factor'
-    p[0] = ('EXPRESSION-TIMES', p[1], p[3])
+    p[0] = ('OCCULT_EXPRESSION-TIMES', p[1], p[3])
 
 def p_term_div(p):
     'term : term DIVIDE factor'
-    p[0] = ('EXPRESSION-DIVIDE', p[1], p[3])
+    p[0] = ('OCCULT_EXPRESSION-DIVIDE', p[1], p[3])
 
 def p_term_factor(p):
     'term : factor'
-    p[0] = ('TERM-FACTOR', p[1])
+    p[0] = ('OCCULT_TERM-FACTOR', p[1])
 
 def p_factor_int(p):
     'factor : INT'
-    p[0] = ('FACTOR-INT', p[1])
+    p[0] = ('OCCULT_FACTOR-INT', p[1])
 
 def p_factor_float(p):
     'factor : FLOAT'
-    p[0] = ('FACTOR-FLOAT', p[1])
+    p[0] = ('OCCULT_FACTOR-FLOAT', p[1])
 
 def p_factor_variable(p):
     'factor : NAME'
-    p[0] = ('FACTOR-VARIABLE', p[1])
+    p[0] = ('OCCULT_FACTOR-VARIABLE', p[1])
 
 def p_factor_array_variable(p):
     'factor : NAME LSQUARE INT RSQUARE'
-    p[0] = ('FACTOR-ARRAY-VARIABLE', p[1], p[3])
+    p[0] = ('OCCULT_FACTOR-ARRAY-VARIABLE', p[1], p[3])
 
 def p_factor_string(p):
     'factor : STRING'
-    p[0] = ('FACTOR-STRING', p[1])
+    p[0] = ('OCCULT_FACTOR-STRING', p[1])
 
 def p_factor_expr(p):
     'factor : LPAREN expression RPAREN'
-    p[0] = ('FACTOR-EXPR', p[2])    
+    p[0] = ('OCCULT_FACTOR-EXPR', p[2])    
 
 def p_error(p):
     if p:
