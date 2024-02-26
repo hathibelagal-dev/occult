244d71d80c1eb836384a4afecf65dd96a78a5378
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2024-02-26 22:52:18 +0100

    json generation

--- occult_gen.py
+++ occult_gen.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 stack = []
 
@@ -7,12 +8,20 @@ _output_file = None
 
 _of = None
 
+def _gen_json(tup):
+    if isinstance(tup, tuple):
+        if len(tup) == 2 and isinstance(tup[0], str):
+            return {tup[0]: _gen_json(tup[1])}
+        else:
+            return [_gen_json(item) for item in tup]
+    else:
+        return tup
+
 def _dfs(item, depth = 0):
     stack.append(item[0])
-    if _output_format == "ast":
-        _of.write(" " * depth * 2)
-        _of.write(item[0])
-        _of.write("\n")
+    _of.write(" " * depth * 2)
+    _of.write(item[0])
+    _of.write("\n")
     for i in range(1, len(item)):
         if type(item[i]) is tuple:
             _dfs(item[i], depth + 1)
@@ -36,8 +45,12 @@ def items(ast, translate_to, output_file):
     if ast[0] != "OCCULT_PROGRAM":
         print("Programma invalidum ☠️ ")
         sys.exit(1)
-    for item in ast[1]:
-        _dfs(item, 0)
-        _process()
-        stack = []
+    if _output_format == 'ast':
+        for item in ast[1]:
+            _dfs(item, 0)
+            _process()
+            stack = []
+    elif _output_format == 'json':
+        json_data = _gen_json(ast)
+        _of.write(json.dumps(json_data, indent = 2))
     _of.close()
